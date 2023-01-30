4a9b12c92cff45d3fa4bd44198194d56922813a0
Vaibhav Parekh <100761402+Vaibhavsp@users.noreply.github.com>
2023-01-30 13:45:19 +0530

    Create code2.cpp

---
+++ code2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for (int k = 0; k < n; k++)
+    {
+        string s,s1;
+        cin>>s;
+        cin>>s1;
+        int count = 0;
+        for (int i = 0; i < s.length(); i++)
+        {
+            for (int j = 0; j < s1.length(); j++)
+            {
+                if (s[i] == s1[j])
+                {
+                    count++;
+                    break;
+                }
+            }
+            if (count == s.length())
+            {
+                cout<<"YES";
+                break;
+            }
+        }
+        if (count < s.length())
+        {
+            cout<<"NO";
+        }
+    }
+}
